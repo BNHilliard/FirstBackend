767e4263b847519f53904e20d92c18740d1fb952
timothy hoover <bnhilliard>
2022-11-10 16:08:54 -0800

    update

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ server.get('/', (req, res) => {
 });
 
 server.get('/jabberwocky', (req, res) => {
-    res.send(`<h1>FRABJOUS<h1><p>${process.env.JABBERWOCKY}</p>`);
+    res.send('<h1>FRABJOUS<h1>');
 });
 
 
